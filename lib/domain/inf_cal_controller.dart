b084f893b3c988868e20cfade30ef7209f2294da
lavruh <lavrsi87@gmail.com>
2024-04-02 21:01:33 +0300

    improve view controller

--- lib/domain/inf_cal_controller.dart
+++ lib/domain/inf_cal_controller.dart
@@ -3,18 +3,17 @@ import 'package:inf_cal/utils/date_extension.dart';
 import 'package:intl/intl.dart';
 
 class InfCalController extends ChangeNotifier {
-  double entryHeight = 30.0;
-  double widgetWidth = 100.0;
-  double scroll = 0.0;
-  int entriesPerScreen = 0;
-  DateTime? bufferStart;
-  DateTime? bufferEnd;
-  DateTime currentDate = DateTime.now();
-  double scaleFactor = 1.0;
-  int viewStartOffsetEntries = 0;
-  int amountOfGeneratedViewItems = 0;
-  DateTime firstEntryOnScreen = DateTime.now();
-  ScaleLevel scaleLevel = ScaleLevel.minutes;
+  double _entryHeight = 20.0;
+  double _widgetWidth = 100.0;
+  double _scroll = 0.0;
+  int _entriesPerScreen = 0;
+  DateTime? _bufferStart;
+  DateTime? _bufferEnd;
+  DateTime _currentDate = DateTime.now();
+  double _scaleFactor = 1.0;
+  int _viewStartOffsetEntries = 0;
+  DateTime _firstEntryOnScreen = DateTime.now();
+  ScaleLevel _scaleLevel = ScaleLevel.minutes;
   bool _zoomMode = false;
   double _mouseScale = 1;
   Duration _iteration = const Duration(minutes: 1);
@@ -28,12 +27,12 @@ class InfCalController extends ChangeNotifier {
   }
 
   void scrollCalendar(double offset) {
-    scroll += offset;
+    _scroll += offset;
     notifyListeners();
   }
 
   void scaleCalendar(double scale) {
-    scaleFactor = scale;
+    _scaleFactor = scale;
     notifyListeners();
   }
 
@@ -44,47 +43,46 @@ class InfCalController extends ChangeNotifier {
   }
 
   void determinateViewPortDatesLimits({required BuildContext context}) {
-    entriesPerScreen = MediaQuery.of(context).size.height ~/ entryHeight;
-    viewStartOffsetEntries = -entriesPerScreen * 3;
-    bufferStart = currentDate.add(_iteration * viewStartOffsetEntries);
-    if (scaleLevel != ScaleLevel.minutes) {
-      bufferStart = bufferStart?.copyWith(minute: 0, second: 0);
+    _entriesPerScreen = MediaQuery.of(context).size.height ~/ _entryHeight;
+    _viewStartOffsetEntries = -_entriesPerScreen * 3;
+    _bufferStart = _currentDate.add(_iteration * _viewStartOffsetEntries);
+    if (_scaleLevel != ScaleLevel.minutes) {
+      _bufferStart = _bufferStart?.copyWith(minute: 0, second: 0);
     }
 
-    bufferEnd = currentDate.add(_iteration * entriesPerScreen * 4);
-    widgetWidth = MediaQuery.of(context).size.width;
+    _bufferEnd = _currentDate.add(_iteration * _entriesPerScreen * 4);
+    _widgetWidth = MediaQuery.of(context).size.width;
   }
 
   List<Widget> updateView() {
     List<Widget> viewBuffer = [];
-    final start = bufferStart;
-    final end = bufferEnd;
-    final scaledHeight = (entryHeight * scaleFactor);
-    final viewStartOffset = viewStartOffsetEntries * scaledHeight;
+    final start = _bufferStart;
+    final end = _bufferEnd;
+    final scaledHeight = (_entryHeight * _scaleFactor);
+    final viewStartOffset = _viewStartOffsetEntries * scaledHeight;
     int i = 0;
 
     if (start == null || end == null) return [];
     for (DateTime d = start;
         d.millisecondsSinceEpoch < end.millisecondsSinceEpoch;
         d = d.add(_iteration)) {
-      final p = scroll + viewStartOffset + i * scaledHeight;
-      if (p + scaledHeight > 0 && p <= scaledHeight) firstEntryOnScreen = d;
+      final p = _scroll + viewStartOffset + i * scaledHeight;
+      if (p + scaledHeight > 0 && p <= scaledHeight) _firstEntryOnScreen = d;
 
-      if (scaleLevel == ScaleLevel.months) {
+      if (_scaleLevel == ScaleLevel.months) {
         viewBuffer.addAll(_generateMonthsView(i, d));
       }
-      if (scaleLevel == ScaleLevel.days) {
+      if (_scaleLevel == ScaleLevel.days) {
         viewBuffer.addAll(_generateDaysView(i, d));
       }
-      if (scaleLevel == ScaleLevel.hours) {
+      if (_scaleLevel == ScaleLevel.hours) {
         viewBuffer.addAll(_generateHoursView(i, d));
       }
-      if (scaleLevel == ScaleLevel.minutes) {
+      if (_scaleLevel == ScaleLevel.minutes) {
         viewBuffer.addAll(_generateMinutesView(i, d));
       }
       i++;
     }
-    amountOfGeneratedViewItems = i;
     return viewBuffer;
   }
 
@@ -98,24 +96,24 @@ class InfCalController extends ChangeNotifier {
     AlignmentGeometry? alignment,
     Color? color,
   }) {
-    final viewStartDate = bufferStart!;
+    final viewStartDate = _bufferStart!;
     final start =
         startDate.millisecondsSinceEpoch > viewStartDate.millisecondsSinceEpoch
             ? startDate
             : viewStartDate;
-    final viewEndDate = bufferEnd!;
+    final viewEndDate = _bufferEnd!;
     final end =
         endDate.microsecondsSinceEpoch < viewEndDate.microsecondsSinceEpoch
             ? endDate
             : viewEndDate;
 
-    final scaledHeight = entryHeight * scaleFactor;
-    final viewStartOffset = viewStartOffsetEntries * scaledHeight;
+    final scaledHeight = _entryHeight * _scaleFactor;
+    final viewStartOffset = _viewStartOffsetEntries * scaledHeight;
     final durationDivider = _iteration.inMicroseconds;
     final daysDiff =
         start.difference(viewStartDate).inMicroseconds ~/ durationDivider;
 
-    final topPosition = scroll + daysDiff * scaledHeight + viewStartOffset;
+    final topPosition = _scroll + daysDiff * scaledHeight + viewStartOffset;
     final height = (end.difference(start).inMicroseconds ~/ durationDivider) *
         scaledHeight;
 
@@ -141,25 +139,25 @@ class InfCalController extends ChangeNotifier {
   }
 
   void switchScaleLevel(int i) {
-    final level = scaleLevel.index;
+    final level = _scaleLevel.index;
     if (level + i < 0 || level + i > ScaleLevel.values.length - 1) return;
-    scaleLevel = ScaleLevel.values[scaleLevel.index + i];
-    entryHeight = 21.0;
-    if (scaleLevel == ScaleLevel.days) _iteration = const Duration(days: 1);
-    if (scaleLevel == ScaleLevel.hours) _iteration = const Duration(hours: 1);
-    if (scaleLevel == ScaleLevel.minutes) {
+    _scaleLevel = ScaleLevel.values[_scaleLevel.index + i];
+    _entryHeight = 21.0;
+    if (_scaleLevel == ScaleLevel.days) _iteration = const Duration(days: 1);
+    if (_scaleLevel == ScaleLevel.hours) _iteration = const Duration(hours: 1);
+    if (_scaleLevel == ScaleLevel.minutes) {
       _iteration = const Duration(minutes: 1);
     }
     updateControllerValues();
   }
 
   void updateControllerValues() {
-    currentDate = firstEntryOnScreen;
-    entryHeight *= scaleFactor;
-    scaleFactor = 1.0;
-    scroll = 0.0;
-    if (entryHeight < 20) switchScaleLevel(1);
-    if (entryHeight > 100) switchScaleLevel(-1);
+    _currentDate = _firstEntryOnScreen;
+    _entryHeight *= _scaleFactor;
+    _scaleFactor = 1.0;
+    _scroll = 0.0;
+    if (_entryHeight < 20) switchScaleLevel(1);
+    if (_entryHeight > 100) switchScaleLevel(-1);
   }
 
   List<Widget> _generateMinutesView(int i, DateTime d) {
@@ -168,7 +166,7 @@ class InfCalController extends ChangeNotifier {
       startDate: d,
       endDate: d.add(_iteration),
       title: DateFormat("HH : mm").format(d),
-      crossDirectionSize: widgetWidth,
+      crossDirectionSize: _widgetWidth,
       crossDirectionOffset: 40,
       alignment: Alignment.centerLeft,
       color: i % 2 == 0 ? Colors.grey.shade50 : null,
@@ -206,7 +204,7 @@ class InfCalController extends ChangeNotifier {
       startDate: d,
       endDate: d.add(_iteration),
       title: DateFormat("HH:00").format(d),
-      crossDirectionSize: widgetWidth,
+      crossDirectionSize: _widgetWidth,
       crossDirectionOffset: 40,
       alignment: Alignment.centerLeft,
       color: i % 2 == 0 ? Colors.grey.shade50 : null,
@@ -241,7 +239,7 @@ class InfCalController extends ChangeNotifier {
       startDate: d,
       endDate: d.add(const Duration(days: 1)),
       title: DateFormat("EEE dd").format(d),
-      crossDirectionSize: widgetWidth,
+      crossDirectionSize: _widgetWidth,
       crossDirectionOffset: 70,
       alignment: Alignment.centerLeft,
       color: i % 2 == 0 ? Colors.grey.shade50 : null,
@@ -298,7 +296,7 @@ class InfCalController extends ChangeNotifier {
         startDate: d,
         endDate: d.add(const Duration(days: 6)),
         title: "wk: ${d.weekNumber()}",
-        crossDirectionSize: widgetWidth,
+        crossDirectionSize: _widgetWidth,
         crossDirectionOffset: 40,
         textDirection: 0,
         alignment: Alignment.topLeft,
